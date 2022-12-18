af55cb9806be486c1bbd1d59f62edca1da1fc363
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2022-12-17 19:49:57 -0700

    Refactor tests for the `bigint` module

--- test/bigint_test.ts
+++ test/bigint_test.ts
@@ -1,31 +1,24 @@
 import { cmp, eq, Ordering } from "@neotype/prelude/cmp.js";
-import { assert } from "chai";
+import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/bigint.js";
 
-describe("bigint", () => {
-    specify("[Eq.eq]", () => {
-        fc.assert(
-            fc.property(fc.bigInt(), fc.bigInt(), (x, y) => {
-                const t0 = eq(x, y);
-                assert.strictEqual(t0, x === y);
-            }),
-        );
-    });
+describe("bigint.js", () => {
+    describe("BigInt", () => {
+        specify("[Eq.eq]", () => {
+            fc.assert(
+                fc.property(fc.bigInt(), fc.bigInt(), (x, y) => {
+                    expect(eq(x, y)).to.equal(x === y);
+                }),
+            );
+        });
 
-    specify("[Ord.cmp]", () => {
-        fc.assert(
-            fc.property(fc.bigInt(), fc.bigInt(), (x, y) => {
-                const t0 = cmp(x, y);
-                assert.strictEqual(
-                    t0,
-                    x < y
-                        ? Ordering.less
-                        : x > y
-                        ? Ordering.greater
-                        : Ordering.equal,
-                );
-            }),
-        );
+        specify("[Ord.cmp]", () => {
+            fc.assert(
+                fc.property(fc.bigInt(), fc.bigInt(), (x, y) => {
+                    expect(cmp(x, y)).to.equal(Ordering.fromNumber(x - y));
+                }),
+            );
+        });
     });
 });
