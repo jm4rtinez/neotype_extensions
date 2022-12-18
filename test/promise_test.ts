4b412c74e3b9dc29b980e073bb460449e85e2c8a
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2022-12-18 13:55:12 -0700

    Refactor tests for the `promise` module

--- test/promise_test.ts
+++ test/promise_test.ts
@@ -1,16 +1,21 @@
 import { cmb } from "@neotype/prelude/cmb.js";
-import { assert } from "chai";
+import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/promise.js";
 import "../src/string.js";
 
-describe("Promise", () => {
-    specify("[Semigroup.cmb]", () => {
-        fc.assert(
-            fc.asyncProperty(fc.string(), fc.string(), async (x, y) => {
-                const t0 = await cmb(Promise.resolve(x), Promise.resolve(y));
-                assert.strictEqual(t0, cmb(x, y));
-            }),
-        );
+describe("promise.js", () => {
+    describe("Promise", () => {
+        specify("#[Semigroup.cmb]", async () => {
+            await fc.assert(
+                fc.asyncProperty(fc.string(), fc.string(), async (x, y) => {
+                    const result = await cmb(
+                        Promise.resolve(x),
+                        Promise.resolve(y),
+                    );
+                    expect(result).to.equal(cmb(x, y));
+                }),
+            );
+        });
     });
 });
