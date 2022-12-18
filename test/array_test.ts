222880acdd81d183588ece69edfc18e4a3ac892c
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2022-12-17 19:46:56 -0700

    Refactor tests for the `array` module

--- test/array_test.ts
+++ test/array_test.ts
@@ -1,120 +1,125 @@
 import { cmb } from "@neotype/prelude/cmb.js";
 import { cmp, eq, icmp, ieq } from "@neotype/prelude/cmp.js";
-import { assert } from "chai";
+import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/array.js";
 import "../src/number.js";
 import "../src/string.js";
 
-describe("Array", () => {
-    specify("[Eq.eq]", () => {
-        fc.assert(
-            fc.property(
-                fc.array(fc.float({ noNaN: true })),
-                fc.array(fc.float({ noNaN: true })),
-                (xs, ys) => {
-                    const t0 = eq(xs, ys);
-                    assert.strictEqual(t0, ieq(xs, ys));
+describe("array.js", () => {
+    describe("Array", () => {
+        specify("[Eq.eq]", () => {
+            fc.assert(
+                fc.property(
+                    fc.array(fc.float({ noNaN: true })),
+                    fc.array(fc.float({ noNaN: true })),
+                    (xs, ys) => {
+                        expect(eq(xs, ys)).to.equal(ieq(xs, ys));
+                    },
+                ),
+            );
+        });
 
-                    const t1 = eq(xs as readonly number[], ys);
-                    assert.strictEqual(t1, ieq(xs, ys));
+        specify("[Ord.cmp]", () => {
+            fc.assert(
+                fc.property(
+                    fc.array(fc.float({ noNaN: true })),
+                    fc.array(fc.float({ noNaN: true })),
+                    (xs, ys) => {
+                        expect(cmp(xs, ys)).to.equal(icmp(xs, ys));
+                    },
+                ),
+            );
+        });
 
-                    const t2 = eq(xs, ys as readonly number[]);
-                    assert.strictEqual(t2, ieq(xs, ys));
-                },
-            ),
-        );
+        specify("[Semigroup.cmb]", () => {
+            fc.assert(
+                fc.property(
+                    fc.array(fc.anything()),
+                    fc.array(fc.anything()),
+                    (xs, ys) => {
+                        expect(cmb(xs, ys)).to.equal([...xs, ...ys]);
+                    },
+                ),
+            );
+        });
     });
 
-    specify("[Ord.cmp]", () => {
-        fc.assert(
-            fc.property(
-                fc.array(fc.float({ noNaN: true })),
-                fc.array(fc.float({ noNaN: true })),
-                (xs, ys) => {
-                    const t0 = cmp(xs, ys);
-                    assert.strictEqual(t0, icmp(xs, ys));
+    describe("ReadonlyArray", () => {
+        specify("[Eq.eq]", () => {
+            const xs: readonly number[] = [];
+            const ys: number[] = [];
+            eq(xs, xs);
+            eq(xs, ys);
+            eq(ys, xs);
+        });
 
-                    const t1 = cmp(xs as readonly number[], ys);
-                    assert.strictEqual(t1, icmp(xs, ys));
+        specify("[Ord.cmp]", () => {
+            const xs: readonly number[] = [];
+            const ys: number[] = [];
+            cmp(xs, xs);
+            cmp(xs, ys);
+            cmp(ys, xs);
+        });
 
-                    const t2 = cmp(xs, ys as readonly number[]);
-                    assert.strictEqual(t2, icmp(xs, ys));
-                },
-            ),
-        );
+        specify("[Semigroup.cmb]", () => {
+            const xs: readonly unknown[] = [];
+            const ys: unknown[] = [];
+            cmb(xs, xs);
+            cmb(xs, ys);
+            cmb(ys, xs);
+        });
     });
 
-    specify("[Semigroup.cmb]", () => {
-        fc.assert(
-            fc.property(
-                fc.array(fc.float({ noNaN: true })),
-                fc.array(fc.float({ noNaN: true })),
-                (xs, ys) => {
-                    const t0 = cmb(xs, ys);
-                    assert.deepEqual(t0, [...xs, ...ys]);
-                },
-            ),
-        );
-    });
-});
+    describe("tuple literal", () => {
+        specify("[Eq.eq]", () => {
+            fc.assert(
+                fc.property(
+                    fc.float({ noNaN: true }),
+                    fc.string(),
+                    fc.float({ noNaN: true }),
+                    fc.string(),
+                    (a, x, b, y) => {
+                        const xs: [number, string] = [a, x];
+                        const ys: [number, string] = [b, y];
+                        expect(eq(xs, ys)).to.equal(eq(a, b) && eq(x, y));
+                    },
+                ),
+            );
+        });
 
-describe("tuple literal", () => {
-    specify("[Eq.eq]", () => {
-        fc.assert(
-            fc.property(
-                fc.float({ noNaN: true }),
-                fc.string(),
-                fc.float({ noNaN: true }),
-                fc.string(),
-                (a, x, b, y) => {
-                    const xs: [number, string] = [a, x];
-                    const ys: [number, string] = [b, y];
-                    const rxs = [a, x] as const;
-                    const rys = [b, y] as const;
-
-                    const t0 = eq(xs, ys);
-                    assert.strictEqual(t0, eq(a, b) && eq(x, y));
-
-                    const t1 = eq(xs, rys);
-                    assert.strictEqual(t1, eq(a, b) && eq(x, y));
-
-                    const t2 = eq(rxs, ys);
-                    assert.strictEqual(t2, eq(a, b) && eq(x, y));
-
-                    const t3 = eq(rxs, ys);
-                    assert.strictEqual(t3, eq(a, b) && eq(x, y));
-                },
-            ),
-        );
+        specify("[Ord.cmp]", () => {
+            fc.assert(
+                fc.property(
+                    fc.float({ noNaN: true }),
+                    fc.string(),
+                    fc.float({ noNaN: true }),
+                    fc.string(),
+                    (a, x, b, y) => {
+                        const xs: [number, string] = [a, x];
+                        const ys: [number, string] = [b, y];
+                        expect(cmp(xs, ys)).to.equal(cmb(cmp(a, b), cmp(x, y)));
+                    },
+                ),
+            );
+        });
     });
 
-    specify("[Ord.cmp]", () => {
-        fc.assert(
-            fc.property(
-                fc.float({ noNaN: true }),
-                fc.string(),
-                fc.float({ noNaN: true }),
-                fc.string(),
-                (a, x, b, y) => {
-                    const xs: [number, string] = [a, x];
-                    const ys: [number, string] = [b, y];
-                    const rxs = [a, x] as const;
-                    const rys = [b, y] as const;
+    describe("readonly tuple literal", () => {
+        specify("[Eq.eq]", () => {
+            const xs: readonly [number, string] = [0, ""];
+            const ys: [number, string] = [0, ""];
+            eq(xs, xs);
+            eq(xs, ys);
+            eq(ys, xs);
+        });
 
-                    const t0 = cmp(xs, ys);
-                    assert.strictEqual(t0, cmb(cmp(a, b), cmp(x, y)));
-
-                    const t1 = cmp(xs, rys);
-                    assert.strictEqual(t1, cmb(cmp(a, b), cmp(x, y)));
-
-                    const t2 = cmp(rxs, ys);
-                    assert.strictEqual(t2, cmb(cmp(a, b), cmp(x, y)));
-
-                    const t3 = cmp(rxs, ys);
-                    assert.strictEqual(t3, cmb(cmp(a, b), cmp(x, y)));
-                },
-            ),
-        );
+        specify("[Ord.cmp]", () => {
+            const xs: readonly [number, string] = [0, ""];
+            const ys: [number, string] = [0, ""];
+            cmp(xs, xs);
+            cmp(xs, ys);
+            cmp(ys, xs);
+        });
     });
 });
