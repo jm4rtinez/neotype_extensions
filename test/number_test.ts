9ab8232537a035a92f987065c798bc92d84de1de
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2022-12-18 13:52:21 -0700

    Refactor tests for the `number` module

--- test/number_test.ts
+++ test/number_test.ts
@@ -1,39 +1,32 @@
 import { cmp, eq, Ordering } from "@neotype/prelude/cmp.js";
-import { assert } from "chai";
+import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/number.js";
 
-describe("number", () => {
-    specify("[Eq.eq]", () => {
-        fc.assert(
-            fc.property(
-                fc.float({ noNaN: true }),
-                fc.float({ noNaN: true }),
-                (x, y) => {
-                    const t0 = eq(x, y);
-                    assert.strictEqual(t0, x === y);
-                },
-            ),
-        );
-    });
+describe("number.js", () => {
+    describe("Number", () => {
+        specify("#[Eq.eq]", () => {
+            fc.assert(
+                fc.property(
+                    fc.float({ noNaN: true }),
+                    fc.float({ noNaN: true }),
+                    (x, y) => {
+                        expect(eq(x, y)).to.equal(x === y);
+                    },
+                ),
+            );
+        });
 
-    specify("[Ord.cmp]", () => {
-        fc.assert(
-            fc.property(
-                fc.float({ noNaN: true }),
-                fc.float({ noNaN: true }),
-                (x, y) => {
-                    const t0 = cmp(x, y);
-                    assert.strictEqual(
-                        t0,
-                        x < y
-                            ? Ordering.less
-                            : x > y
-                            ? Ordering.greater
-                            : Ordering.equal,
-                    );
-                },
-            ),
-        );
+        specify("#[Ord.cmp]", () => {
+            fc.assert(
+                fc.property(
+                    fc.float({ noNaN: true }),
+                    fc.float({ noNaN: true }),
+                    (x, y) => {
+                        expect(cmp(x, y)).to.equal(Ordering.fromNumber(x - y));
+                    },
+                ),
+            );
+        });
     });
 });
