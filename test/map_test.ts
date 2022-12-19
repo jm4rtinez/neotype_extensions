53e1fa45faf5ff2fb4fddd12492cfd05a9bcd55b
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2022-12-18 17:18:44 -0700

    Require an `Eq` implemention from values in maps
    For the `Map` and `ReadonlyMap` equivalence relations, require that the
    values also implement `Eq`.

--- test/map_test.ts
+++ test/map_test.ts
@@ -3,6 +3,7 @@ import { eq } from "@neotype/prelude/cmp.js";
 import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/map.js";
+import "../src/string.js";
 
 describe("map.js", () => {
     describe("Map", () => {
@@ -10,10 +11,10 @@ describe("map.js", () => {
             fc.assert(
                 fc.property(
                     fc
-                        .array(fc.tuple(fc.anything(), fc.anything()))
+                        .array(fc.tuple(fc.anything(), fc.string()))
                         .map((entries) => new Map(entries)),
                     fc
-                        .array(fc.tuple(fc.anything(), fc.anything()))
+                        .array(fc.tuple(fc.anything(), fc.string()))
                         .map((entries) => new Map(entries)),
                     (xs, ys) => {
                         const result = eq(xs, ys);
@@ -23,7 +24,8 @@ describe("map.js", () => {
                                 return false;
                             }
                             for (const [kx, x] of xs.entries()) {
-                                if (!(ys.has(kx) && ys.get(kx) === x)) {
+                                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                                if (!(ys.has(kx) && eq(ys.get(kx)!, x))) {
                                     return false;
                                 }
                             }
@@ -62,8 +64,8 @@ describe("map.js", () => {
 
     describe("ReadonlyMap", () => {
         specify("#[Eq.eq]", () => {
-            const xs: ReadonlyMap<unknown, unknown> = new Map();
-            const ys: Map<unknown, unknown> = new Map();
+            const xs: ReadonlyMap<unknown, string> = new Map();
+            const ys: Map<unknown, string> = new Map();
             eq(xs, xs);
             eq(xs, ys);
             eq(ys, xs);
