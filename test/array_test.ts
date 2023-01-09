6c72a0cd5a6f423dd7a8d67101edebd97741b618
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-08 20:59:50 -0700

    Add test titles and lawfulness tests for `array`

--- test/array_test.ts
+++ test/array_test.ts
@@ -5,121 +5,172 @@ import * as fc from "fast-check";
 import "../src/array.js";
 import "../src/number.js";
 import "../src/string.js";
+import {
+    expectLawfulEq,
+    expectLawfulOrd,
+    expectLawfulSemigroup,
+} from "./util.js";
 
 describe("array.js", () => {
     describe("Array", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(
-                    fc.array(fc.float({ noNaN: true })),
-                    fc.array(fc.float({ noNaN: true })),
-                    (xs, ys) => {
-                        expect(eq(xs, ys)).to.equal(ieq(xs, ys));
-                    },
-                ),
-            );
+        describe("#[Eq.eq]", () => {
+            it("compares arrays lexicographically", () => {
+                fc.assert(
+                    fc.property(
+                        fc.array(fc.float({ noNaN: true })),
+                        fc.array(fc.float({ noNaN: true })),
+                        (xs, ys) => {
+                            expect(eq(xs, ys)).to.equal(ieq(xs, ys));
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(fc.array(fc.float({ noNaN: true })));
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            fc.assert(
-                fc.property(
-                    fc.array(fc.float({ noNaN: true })),
-                    fc.array(fc.float({ noNaN: true })),
-                    (xs, ys) => {
-                        expect(cmp(xs, ys)).to.equal(icmp(xs, ys));
-                    },
-                ),
-            );
+        describe("#[Ord.cmp]", () => {
+            it("compares arrays lexicographically", () => {
+                fc.assert(
+                    fc.property(
+                        fc.array(fc.float({ noNaN: true })),
+                        fc.array(fc.float({ noNaN: true })),
+                        (xs, ys) => {
+                            expect(cmp(xs, ys)).to.equal(icmp(xs, ys));
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful total order", () => {
+                expectLawfulOrd(fc.array(fc.float({ noNaN: true })));
+            });
         });
 
-        specify("#[Semigroup.cmb]", () => {
-            fc.assert(
-                fc.property(
-                    fc.array(fc.anything()),
-                    fc.array(fc.anything()),
-                    (xs, ys) => {
-                        expect(cmb(xs, ys)).to.deep.equal([...xs, ...ys]);
-                    },
-                ),
-            );
+        describe("#[Semigroup.cmb]", () => {
+            it("combines arrays using concatenation", () => {
+                fc.assert(
+                    fc.property(
+                        fc.array(fc.anything()),
+                        fc.array(fc.anything()),
+                        (xs, ys) => {
+                            expect(cmb(xs, ys)).to.deep.equal([...xs, ...ys]);
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful semigroup", () => {
+                expectLawfulSemigroup(fc.array(fc.anything()));
+            });
         });
     });
 
     describe("ReadonlyArray", () => {
-        specify("#[Eq.eq]", () => {
-            const xs: readonly number[] = [];
-            const ys: number[] = [];
-            eq(xs, xs);
-            eq(xs, ys);
-            eq(ys, xs);
+        describe("#[Eq.eq]", () => {
+            it("compares readonly and non-readonly arrays to each other", () => {
+                const xs: readonly number[] = [];
+                const ys: number[] = [];
+                eq(xs, xs);
+                eq(xs, ys);
+                eq(ys, xs);
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            const xs: readonly number[] = [];
-            const ys: number[] = [];
-            cmp(xs, xs);
-            cmp(xs, ys);
-            cmp(ys, xs);
+        describe("#[Ord.cmp]", () => {
+            it("compares readonly and non-readonly arrays to each other", () => {
+                const xs: readonly number[] = [];
+                const ys: number[] = [];
+                cmp(xs, xs);
+                cmp(xs, ys);
+                cmp(ys, xs);
+            });
         });
 
-        specify("#[Semigroup.cmb]", () => {
-            const xs: readonly unknown[] = [];
-            const ys: unknown[] = [];
-            cmb(xs, xs);
-            cmb(xs, ys);
-            cmb(ys, xs);
+        describe("#[Semigroup.cmb]", () => {
+            it("combines readonly and non-readonly arrays with each other", () => {
+                const xs: readonly unknown[] = [];
+                const ys: unknown[] = [];
+                cmb(xs, xs);
+                cmb(xs, ys);
+                cmb(ys, xs);
+            });
         });
     });
 
     describe("tuple literal", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(
-                    fc.float({ noNaN: true }),
-                    fc.string(),
-                    fc.float({ noNaN: true }),
-                    fc.string(),
-                    (a, x, b, y) => {
-                        const xs: [number, string] = [a, x];
-                        const ys: [number, string] = [b, y];
-                        expect(eq(xs, ys)).to.equal(eq(a, b) && eq(x, y));
-                    },
-                ),
-            );
+        describe("#[Eq.eq]", () => {
+            it("compares tuple literals lexicographically", () => {
+                fc.assert(
+                    fc.property(
+                        fc.float({ noNaN: true }),
+                        fc.string(),
+                        fc.float({ noNaN: true }),
+                        fc.string(),
+                        (a, x, b, y) => {
+                            const xs: [number, string] = [a, x];
+                            const ys: [number, string] = [b, y];
+                            expect(eq(xs, ys)).to.equal(eq(a, b) && eq(x, y));
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(
+                    fc.tuple(fc.float({ noNaN: true }), fc.string()),
+                );
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            fc.assert(
-                fc.property(
-                    fc.float({ noNaN: true }),
-                    fc.string(),
-                    fc.float({ noNaN: true }),
-                    fc.string(),
-                    (a, x, b, y) => {
-                        const xs: [number, string] = [a, x];
-                        const ys: [number, string] = [b, y];
-                        expect(cmp(xs, ys)).to.equal(cmb(cmp(a, b), cmp(x, y)));
-                    },
-                ),
-            );
+        describe("#[Ord.cmp]", () => {
+            it("compares tuple literals lexicographically", () => {
+                fc.assert(
+                    fc.property(
+                        fc.float({ noNaN: true }),
+                        fc.string(),
+                        fc.float({ noNaN: true }),
+                        fc.string(),
+                        (a, x, b, y) => {
+                            const xs: [number, string] = [a, x];
+                            const ys: [number, string] = [b, y];
+                            expect(cmp(xs, ys)).to.equal(
+                                cmb(cmp(a, b), cmp(x, y)),
+                            );
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful total order", () => {
+                expectLawfulOrd(
+                    fc.tuple(fc.float({ noNaN: true }), fc.string()),
+                );
+            });
         });
     });
 
     describe("readonly tuple literal", () => {
-        specify("#[Eq.eq]", () => {
-            const xs: readonly [number, string] = [0, ""];
-            const ys: [number, string] = [0, ""];
-            eq(xs, xs);
-            eq(xs, ys);
-            eq(ys, xs);
+        describe("#[Eq.eq]", () => {
+            it("compares readonly and non-readonly tuple literals to each other", () => {
+                const xs: readonly [number, string] = [0, ""];
+                const ys: [number, string] = [0, ""];
+                eq(xs, xs);
+                eq(xs, ys);
+                eq(ys, xs);
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            const xs: readonly [number, string] = [0, ""];
-            const ys: [number, string] = [0, ""];
-            cmp(xs, xs);
-            cmp(xs, ys);
-            cmp(ys, xs);
+        describe("#[Ord.cmp]", () => {
+            it("compares readonly and non-readonly tuple literals to each other", () => {
+                const xs: readonly [number, string] = [0, ""];
+                const ys: [number, string] = [0, ""];
+                cmp(xs, xs);
+                cmp(xs, ys);
+                cmp(ys, xs);
+            });
         });
     });
 });
