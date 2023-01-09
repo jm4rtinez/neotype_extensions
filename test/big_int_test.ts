58bb888bb61d51226897d0a3847e7a53e3df4255
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-08 21:06:59 -0700

    Add test titles and lawfulness tests for `big_int`

--- test/big_int_test.ts
+++ test/big_int_test.ts
@@ -2,23 +2,36 @@ import { cmp, eq, Ordering } from "@neotype/prelude/cmp.js";
 import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/big_int.js";
+import { expectLawfulEq, expectLawfulOrd } from "./util.js";
 
 describe("big_int.js", () => {
     describe("BigInt", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(fc.bigInt(), fc.bigInt(), (x, y) => {
-                    expect(eq(x, y)).to.equal(x === y);
-                }),
-            );
+        describe("#[Eq.eq]", () => {
+            it("compares bigint values strictly", () => {
+                fc.assert(
+                    fc.property(fc.bigInt(), fc.bigInt(), (x, y) => {
+                        expect(eq(x, y)).to.equal(x === y);
+                    }),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(fc.bigInt());
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            fc.assert(
-                fc.property(fc.bigInt(), fc.bigInt(), (x, y) => {
-                    expect(cmp(x, y)).to.equal(Ordering.fromNumber(x - y));
-                }),
-            );
+        describe("#[Ord.cmp]", () => {
+            it("compares bigint values as ordered from least to greatest", () => {
+                fc.assert(
+                    fc.property(fc.bigInt(), fc.bigInt(), (x, y) => {
+                        expect(cmp(x, y)).to.equal(Ordering.fromNumber(x - y));
+                    }),
+                );
+            });
+
+            it("implements a lawful total order", () => {
+                expectLawfulOrd(fc.bigInt());
+            });
         });
     });
 });
