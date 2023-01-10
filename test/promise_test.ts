f3da537e284da016d7a8011f6aa35fc519fca832
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 09:21:14 -0700

    Add titles and lawfulness tests for `promise`

--- test/promise_test.ts
+++ test/promise_test.ts
@@ -1,4 +1,4 @@
-import { cmb } from "@neotype/prelude/cmb.js";
+import { cmb, type Semigroup } from "@neotype/prelude/cmb.js";
 import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/promise.js";
@@ -6,16 +6,36 @@ import "../src/string.js";
 
 describe("promise.js", () => {
     describe("Promise", () => {
-        specify("#[Semigroup.cmb]", async () => {
-            await fc.assert(
-                fc.asyncProperty(fc.string(), fc.string(), async (x, y) => {
-                    const result = await cmb(
-                        Promise.resolve(x),
-                        Promise.resolve(y),
+        describe("#[Semigroup.cmb]", () => {
+            it("combines the fulfilled results", async () => {
+                await fc.assert(
+                    fc.asyncProperty(fc.string(), fc.string(), async (x, y) => {
+                        const result = await cmb(
+                            Promise.resolve(x),
+                            Promise.resolve(y),
+                        );
+                        expect(result).to.equal(cmb(x, y));
+                    }),
+                );
+            });
+
+            it("implements a lawful semigroup", async () => {
+                async function expectLawfulAsyncSemigroup<
+                    A extends Semigroup<A>,
+                >(arb: fc.Arbitrary<Promise<A>>): Promise<void> {
+                    await fc.assert(
+                        fc.asyncProperty(arb, arb, arb, async (x, y, z) => {
+                            const t0 = await cmb(x, cmb(y, z));
+                            const t1 = await cmb(cmb(x, y), z);
+                            expect(t0, "associativity").to.deep.equal(t1);
+                        }),
                     );
-                    expect(result).to.equal(cmb(x, y));
-                }),
-            );
+                }
+
+                await expectLawfulAsyncSemigroup(
+                    fc.string().map((x) => Promise.resolve(x)),
+                );
+            });
         });
     });
 });
