98b47fff33ccd694a712a2bc2216213c6d525c33
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 09:07:37 -0700

    Add titles and lawfulness tests for `number`

--- test/number_test.ts
+++ test/number_test.ts
@@ -2,31 +2,46 @@ import { cmp, eq, Ordering } from "@neotype/prelude/cmp.js";
 import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/number.js";
+import { expectLawfulEq, expectLawfulOrd } from "./util.js";
 
 describe("number.js", () => {
     describe("Number", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(
-                    fc.float({ noNaN: true }),
-                    fc.float({ noNaN: true }),
-                    (x, y) => {
-                        expect(eq(x, y)).to.equal(x === y);
-                    },
-                ),
-            );
+        describe("#[Eq.eq]", () => {
+            it("compares the number values strictly", () => {
+                fc.assert(
+                    fc.property(
+                        fc.float({ noNaN: true }),
+                        fc.float({ noNaN: true }),
+                        (x, y) => {
+                            expect(eq(x, y)).to.equal(x === y);
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(fc.float({ noNaN: true }));
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            fc.assert(
-                fc.property(
-                    fc.float({ noNaN: true }),
-                    fc.float({ noNaN: true }),
-                    (x, y) => {
-                        expect(cmp(x, y)).to.equal(Ordering.fromNumber(x - y));
-                    },
-                ),
-            );
+        describe("#[Ord.cmp]", () => {
+            it("compares the number values as ordered from least to greatest", () => {
+                fc.assert(
+                    fc.property(
+                        fc.float({ noNaN: true }),
+                        fc.float({ noNaN: true }),
+                        (x, y) => {
+                            expect(cmp(x, y)).to.equal(
+                                Ordering.fromNumber(x - y),
+                            );
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful total order", () => {
+                expectLawfulOrd(fc.float({ noNaN: true }));
+            });
         });
     });
 });
