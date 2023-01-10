7d7fb7b5f615c2018a6f6329707ab7035ecb323e
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 08:48:30 -0700

    Add titles and lawfulness tests for `function`

--- test/function_test.ts
+++ test/function_test.ts
@@ -4,17 +4,24 @@ import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/function.js";
 import "../src/string.js";
+import { expectLawfulEq } from "./util.js";
 
 describe("function.js", () => {
     describe("Function", () => {
-        specify("#[Semigroup.cmb]", () => {
-            fc.assert(
-                fc.property(fc.string(), (x) => {
-                    const f = cmb(id<string>, id);
-                    const result = f(x);
-                    expect(result).to.equal(cmb(x, x));
-                }),
-            );
+        describe("#[Semigroup.cmb]", () => {
+            it("combines the functions by returning a function that combines their results", () => {
+                fc.assert(
+                    fc.property(fc.string(), (x) => {
+                        const f = cmb(id<string>, id);
+                        const result = f(x);
+                        expect(result).to.equal(cmb(x, x));
+                    }),
+                );
+            });
+
+            it("implements a lawful semigroup", () => {
+                expectLawfulEq(fc.string().map(id));
+            });
         });
     });
 });
