6861f726c44d95e75d09efefa49a1df2ec7c9414
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 08:34:44 -0700

    Add titles and lawfulness tests for `date`

--- test/date_test.ts
+++ test/date_test.ts
@@ -2,25 +2,38 @@ import { cmp, eq, Ordering } from "@neotype/prelude/cmp.js";
 import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/date.js";
+import { expectLawfulEq, expectLawfulOrd } from "./util.js";
 
 describe("date.js", () => {
     describe("Date", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(fc.date(), fc.date(), (x, y) => {
-                    expect(eq(x, y)).to.equal(x.getTime() === y.getTime());
-                }),
-            );
+        describe("#[Eq.eq]", () => {
+            it("compares Date values using their numerical representation", () => {
+                fc.assert(
+                    fc.property(fc.date(), fc.date(), (x, y) => {
+                        expect(eq(x, y)).to.equal(x.getTime() === y.getTime());
+                    }),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(fc.date());
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            fc.assert(
-                fc.property(fc.date(), fc.date(), (x, y) => {
-                    expect(cmp(x, y)).to.equal(
-                        Ordering.fromNumber(x.getTime() - y.getTime()),
-                    );
-                }),
-            );
+        describe("#[Ord.cmp]", () => {
+            it("compares Date values using their numerical representation", () => {
+                fc.assert(
+                    fc.property(fc.date(), fc.date(), (x, y) => {
+                        expect(cmp(x, y)).to.equal(
+                            Ordering.fromNumber(x.getTime() - y.getTime()),
+                        );
+                    }),
+                );
+            });
+
+            it("implements a lawful total order", () => {
+                expectLawfulOrd(fc.date());
+            });
         });
     });
 });
