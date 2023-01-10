aca04791a4a6ca932ceb6280249190b9aeba53b7
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 14:48:28 -0700

    Add titles and lawfulness tests for `map` and `set`

--- test/map_test.ts
+++ test/map_test.ts
@@ -4,79 +4,104 @@ import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/map.js";
 import "../src/string.js";
+import { expectLawfulEq, expectLawfulSemigroup } from "./util.js";
 
 describe("map.js", () => {
     describe("Map", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(
-                    fc
-                        .array(fc.tuple(fc.anything(), fc.string()))
-                        .map((entries) => new Map(entries)),
-                    fc
-                        .array(fc.tuple(fc.anything(), fc.string()))
-                        .map((entries) => new Map(entries)),
-                    (xs, ys) => {
-                        const result = eq(xs, ys);
+        describe("#[Eq.eq]", () => {
+            it("compares the keys strictly and the values using their Eq implementation", () => {
+                fc.assert(
+                    fc.property(
+                        fc
+                            .array(fc.tuple(fc.anything(), fc.string()))
+                            .map((entries) => new Map(entries)),
+                        fc
+                            .array(fc.tuple(fc.anything(), fc.string()))
+                            .map((entries) => new Map(entries)),
+                        (xs, ys) => {
+                            const result = eq(xs, ys);
 
-                        const exp = (() => {
-                            if (xs.size !== ys.size) {
-                                return false;
-                            }
-                            for (const [kx, x] of xs.entries()) {
-                                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                                if (!(ys.has(kx) && eq(ys.get(kx)!, x))) {
+                            const exp = (() => {
+                                if (xs.size !== ys.size) {
                                     return false;
                                 }
-                            }
-                            return true;
-                        })();
+                                for (const [kx, x] of xs.entries()) {
+                                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                                    if (!(ys.has(kx) && eq(ys.get(kx)!, x))) {
+                                        return false;
+                                    }
+                                }
+                                return true;
+                            })();
 
-                        expect(result).to.equal(exp);
-                    },
-                ),
-            );
+                            expect(result).to.equal(exp);
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(
+                    fc
+                        .array(fc.tuple(fc.anything(), fc.string()))
+                        .map((entries) => new Map(entries)),
+                );
+            });
         });
 
-        specify("#[Semigroup.cmb]", () => {
-            fc.assert(
-                fc.property(
-                    fc
-                        .array(fc.tuple(fc.anything(), fc.anything()))
-                        .map((entries) => new Map(entries)),
-                    fc
-                        .array(fc.tuple(fc.anything(), fc.anything()))
-                        .map((entries) => new Map(entries)),
-                    (xs, ys) => {
-                        const result = cmb(xs, ys);
-                        const exp = new Map([...xs, ...ys]);
+        describe("#[Semigroup.cmb]", () => {
+            it("combines the maps by taking their union", () => {
+                fc.assert(
+                    fc.property(
+                        fc
+                            .array(fc.tuple(fc.anything(), fc.anything()))
+                            .map((entries) => new Map(entries)),
+                        fc
+                            .array(fc.tuple(fc.anything(), fc.anything()))
+                            .map((entries) => new Map(entries)),
+                        (xs, ys) => {
+                            const result = cmb(xs, ys);
+                            const exp = new Map([...xs, ...ys]);
 
-                        expect(result.size).to.equal(exp.size);
-                        for (const [kx, x] of result) {
-                            expect(exp.has(kx)).to.be.true;
-                            expect(exp.get(kx)).to.deep.equal(x);
-                        }
-                    },
-                ),
-            );
+                            expect(result.size).to.equal(exp.size);
+                            for (const [kx, x] of result) {
+                                expect(exp.has(kx)).to.be.true;
+                                expect(exp.get(kx)).to.deep.equal(x);
+                            }
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful semigroup", () => {
+                expectLawfulSemigroup(
+                    fc
+                        .array(fc.tuple(fc.anything(), fc.anything()))
+                        .map((entries) => new Map(entries)),
+                );
+            });
         });
     });
 
     describe("ReadonlyMap", () => {
-        specify("#[Eq.eq]", () => {
-            const xs: ReadonlyMap<unknown, string> = new Map();
-            const ys: Map<unknown, string> = new Map();
-            eq(xs, xs);
-            eq(xs, ys);
-            eq(ys, xs);
+        describe("#[Eq.eq]", () => {
+            it("compares the readonly map and non-readonly map to each other", () => {
+                const xs: ReadonlyMap<unknown, string> = new Map();
+                const ys: Map<unknown, string> = new Map();
+                eq(xs, xs);
+                eq(xs, ys);
+                eq(ys, xs);
+            });
         });
 
-        specify("#[Semigroup.cmb]", () => {
-            const xs: ReadonlyMap<unknown, unknown> = new Map();
-            const ys: Map<unknown, unknown> = new Map();
-            cmb(xs, xs);
-            cmb(xs, ys);
-            cmb(ys, xs);
+        describe("#[Semigroup.cmb]", () => {
+            it("combines the readonly map and non-readonly map with each other", () => {
+                const xs: ReadonlyMap<unknown, unknown> = new Map();
+                const ys: Map<unknown, unknown> = new Map();
+                cmb(xs, xs);
+                cmb(xs, ys);
+                cmb(ys, xs);
+            });
         });
     });
 });
