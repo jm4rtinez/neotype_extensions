f4e4aa55a9876c5178debc36fc7146162fdfbbc3
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 14:24:04 -0700

    Add titles and lawfulness tests for `uint8_array`

--- test/uint8_array_test.ts
+++ test/uint8_array_test.ts
@@ -3,42 +3,67 @@ import { cmp, eq, icmpBy, ieqBy, Ordering } from "@neotype/prelude/cmp.js";
 import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/uint8_array.js";
+import {
+    expectLawfulEq,
+    expectLawfulOrd,
+    expectLawfulSemigroup,
+} from "./util.js";
 
 describe("uint8_array.js", () => {
     describe("Uint8Array", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(fc.uint8Array(), fc.uint8Array(), (xs, ys) => {
-                    expect(eq(xs, ys)).to.equal(
-                        ieqBy(xs, ys, (x, y) => x === y),
-                    );
-                }),
-            );
+        describe("#[Eq.eq]", () => {
+            it("compares the arrays lexicographically", () => {
+                fc.assert(
+                    fc.property(fc.uint8Array(), fc.uint8Array(), (xs, ys) => {
+                        expect(eq(xs, ys)).to.equal(
+                            ieqBy(xs, ys, (x, y) => x === y),
+                        );
+                    }),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(fc.uint8Array());
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            fc.assert(
-                fc.property(fc.uint8Array(), fc.uint8Array(), (xs, ys) => {
-                    expect(cmp(xs, ys)).to.equal(
-                        icmpBy(xs, ys, (x, y) => Ordering.fromNumber(x - y)),
-                    );
-                }),
-            );
+        describe("#[Ord.cmp]", () => {
+            it("compares the arrays lexicographically", () => {
+                fc.assert(
+                    fc.property(fc.uint8Array(), fc.uint8Array(), (xs, ys) => {
+                        expect(cmp(xs, ys)).to.equal(
+                            icmpBy(xs, ys, (x, y) =>
+                                Ordering.fromNumber(x - y),
+                            ),
+                        );
+                    }),
+                );
+            });
+
+            it("implements a lawful total order", () => {
+                expectLawfulOrd(fc.uint8Array());
+            });
         });
 
-        specify("#[Semigroup.cmb]", () => {
-            fc.assert(
-                fc.property(fc.uint8Array(), fc.uint8Array(), (xs, ys) => {
-                    const result = cmb(xs, ys);
+        describe("#[Semigroup.cmb]", () => {
+            it("combines the arrays using concatenation", () => {
+                fc.assert(
+                    fc.property(fc.uint8Array(), fc.uint8Array(), (xs, ys) => {
+                        const result = cmb(xs, ys);
 
-                    const exp = new Uint8Array(xs.length + ys.length);
-                    exp.set(xs);
-                    exp.set(ys, xs.length);
+                        const exp = new Uint8Array(xs.length + ys.length);
+                        exp.set(xs);
+                        exp.set(ys, xs.length);
 
-                    expect(result).to.deep.equal(exp);
-                    expect(exp.length).to.equal(xs.length + ys.length);
-                }),
-            );
+                        expect(result).to.deep.equal(exp);
+                        expect(exp.length).to.equal(xs.length + ys.length);
+                    }),
+                );
+            });
+
+            it("implements a lawful semigroup", () => {
+                expectLawfulSemigroup(fc.uint8Array());
+            });
         });
     });
 });
