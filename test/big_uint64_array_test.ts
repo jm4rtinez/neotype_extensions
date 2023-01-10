475887f77d5ce194335a654e45972cb179925528
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 08:23:55 -0700

    Add titles and lawfulness tests for `big_uint64_array`

--- test/big_uint64_array_test.ts
+++ test/big_uint64_array_test.ts
@@ -3,56 +3,81 @@ import { cmp, eq, icmpBy, ieqBy, Ordering } from "@neotype/prelude/cmp.js";
 import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/big_uint64_array.js";
+import {
+    expectLawfulEq,
+    expectLawfulOrd,
+    expectLawfulSemigroup,
+} from "./util.js";
 
 describe("big_uint64_array.js", () => {
     describe("BigUint64Array", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(
-                    fc.bigUint64Array(),
-                    fc.bigUint64Array(),
-                    (xs, ys) => {
-                        expect(eq(xs, ys)).to.equal(
-                            ieqBy(xs, ys, (x, y) => x === y),
-                        );
-                    },
-                ),
-            );
+        describe("#[Eq.eq]", () => {
+            it("compares the arrays lexicographically", () => {
+                fc.assert(
+                    fc.property(
+                        fc.bigUint64Array(),
+                        fc.bigUint64Array(),
+                        (xs, ys) => {
+                            expect(eq(xs, ys)).to.equal(
+                                ieqBy(xs, ys, (x, y) => x === y),
+                            );
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(fc.bigUint64Array());
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            fc.assert(
-                fc.property(
-                    fc.bigUint64Array(),
-                    fc.bigUint64Array(),
-                    (xs, ys) => {
-                        expect(cmp(xs, ys)).to.equal(
-                            icmpBy(xs, ys, (x, y) =>
-                                Ordering.fromNumber(x - y),
-                            ),
-                        );
-                    },
-                ),
-            );
+        describe("#[Ord.cmp]", () => {
+            it("compares the arrays lexicograpically", () => {
+                fc.assert(
+                    fc.property(
+                        fc.bigUint64Array(),
+                        fc.bigUint64Array(),
+                        (xs, ys) => {
+                            expect(cmp(xs, ys)).to.equal(
+                                icmpBy(xs, ys, (x, y) =>
+                                    Ordering.fromNumber(x - y),
+                                ),
+                            );
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful total order", () => {
+                expectLawfulOrd(fc.bigInt64Array());
+            });
         });
 
-        specify("#[Semigroup.cmb]", () => {
-            fc.assert(
-                fc.property(
-                    fc.bigUint64Array(),
-                    fc.bigUint64Array(),
-                    (xs, ys) => {
-                        const result = cmb(xs, ys);
+        describe("#[Semigroup.cmb]", () => {
+            it("combines the arrays using concatenation", () => {
+                fc.assert(
+                    fc.property(
+                        fc.bigUint64Array(),
+                        fc.bigUint64Array(),
+                        (xs, ys) => {
+                            const result = cmb(xs, ys);
 
-                        const exp = new BigUint64Array(xs.length + ys.length);
-                        exp.set(xs);
-                        exp.set(ys, xs.length);
+                            const exp = new BigUint64Array(
+                                xs.length + ys.length,
+                            );
+                            exp.set(xs);
+                            exp.set(ys, xs.length);
 
-                        expect(result).to.deep.equal(exp);
-                        expect(exp.length).to.equal(xs.length + ys.length);
-                    },
-                ),
-            );
+                            expect(result).to.deep.equal(exp);
+                            expect(exp.length).to.equal(xs.length + ys.length);
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawfulSemigroup", () => {
+                expectLawfulSemigroup(fc.bigInt64Array());
+            });
         });
     });
 });
