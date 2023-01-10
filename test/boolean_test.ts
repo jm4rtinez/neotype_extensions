8178a26d023772e864dc0768c8579979fc0ba9fe
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 08:31:50 -0700

    Add titles and lawfulness tests for `boolean`

--- test/boolean_test.ts
+++ test/boolean_test.ts
@@ -2,25 +2,38 @@ import { cmp, eq, Ordering } from "@neotype/prelude/cmp.js";
 import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/boolean.js";
+import { expectLawfulEq, expectLawfulOrd } from "./util.js";
 
 describe("boolean.js", () => {
     describe("Boolean", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(fc.boolean(), fc.boolean(), (x, y) => {
-                    expect(eq(x, y)).to.equal(x === y);
-                }),
-            );
+        describe("#[Eq.eq]", () => {
+            it("compares the boolean values strictly", () => {
+                fc.assert(
+                    fc.property(fc.boolean(), fc.boolean(), (x, y) => {
+                        expect(eq(x, y)).to.equal(x === y);
+                    }),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(fc.boolean());
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            fc.assert(
-                fc.property(fc.boolean(), fc.boolean(), (x, y) => {
-                    expect(cmp(x, y)).to.equal(
-                        Ordering.fromNumber(Number(x) - Number(y)),
-                    );
-                }),
-            );
+        describe("#[Ord.cmp]", () => {
+            it("compares false as less than true", () => {
+                fc.assert(
+                    fc.property(fc.boolean(), fc.boolean(), (x, y) => {
+                        expect(cmp(x, y)).to.equal(
+                            Ordering.fromNumber(Number(x) - Number(y)),
+                        );
+                    }),
+                );
+            });
+
+            it("implements a lawful total order", () => {
+                expectLawfulOrd(fc.boolean());
+            });
         });
     });
 });
