3df4c9740cb1cd6aebf4b115b9518211e6ee4676
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 14:21:48 -0700

    Add lawfulness test for `symbol`

--- test/symbol_test.ts
+++ test/symbol_test.ts
@@ -1,6 +1,8 @@
 import { eq } from "@neotype/prelude/cmp.js";
 import { expect } from "chai";
+import * as fc from "fast-check";
 import "../src/symbol.js";
+import { expectLawfulEq } from "./util.js";
 
 describe("symbol.js", () => {
     describe("Symbol", () => {
@@ -12,6 +14,18 @@ describe("symbol.js", () => {
 
             it("compares two different symbols as inequal", () => {
                 expect(eq(Symbol(), Symbol())).to.be.false;
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                function arbSymbol(): fc.Arbitrary<symbol> {
+                    return fc.oneof(
+                        fc.constant(Symbol()),
+                        fc.constant(Symbol()),
+                        fc.constant(Symbol()),
+                    );
+                }
+
+                expectLawfulEq(arbSymbol());
             });
         });
     });
