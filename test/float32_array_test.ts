5835f961c7f8d642d6b25ebe926d6af158ae46f4
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-01-10 08:37:47 -0700

    Add titles and lawfulness tests for `float32_array`

--- test/float32_array_test.ts
+++ test/float32_array_test.ts
@@ -3,42 +3,79 @@ import { cmp, eq, icmpBy, ieqBy, Ordering } from "@neotype/prelude/cmp.js";
 import { expect } from "chai";
 import * as fc from "fast-check";
 import "../src/float32_array.js";
+import {
+    expectLawfulEq,
+    expectLawfulOrd,
+    expectLawfulSemigroup,
+} from "./util.js";
 
 describe("float32_array.js", () => {
     describe("Float32Array", () => {
-        specify("#[Eq.eq]", () => {
-            fc.assert(
-                fc.property(fc.float32Array(), fc.float32Array(), (xs, ys) => {
-                    expect(eq(xs, ys)).to.equal(
-                        ieqBy(xs, ys, (x, y) => x === y),
-                    );
-                }),
-            );
+        describe("#[Eq.eq]", () => {
+            it("compares the arrays lexicographically", () => {
+                fc.assert(
+                    fc.property(
+                        fc.float32Array(),
+                        fc.float32Array(),
+                        (xs, ys) => {
+                            expect(eq(xs, ys)).to.equal(
+                                ieqBy(xs, ys, (x, y) => x === y),
+                            );
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful equivalence relation", () => {
+                expectLawfulEq(fc.float32Array());
+            });
         });
 
-        specify("#[Ord.cmp]", () => {
-            fc.assert(
-                fc.property(fc.float32Array(), fc.float32Array(), (xs, ys) => {
-                    expect(cmp(xs, ys)).to.equal(
-                        icmpBy(xs, ys, (x, y) => Ordering.fromNumber(x - y)),
-                    );
-                }),
-            );
+        describe("#[Ord.cmp]", () => {
+            it("compares the arrays lexicographically", () => {
+                fc.assert(
+                    fc.property(
+                        fc.float32Array(),
+                        fc.float32Array(),
+                        (xs, ys) => {
+                            expect(cmp(xs, ys)).to.equal(
+                                icmpBy(xs, ys, (x, y) =>
+                                    Ordering.fromNumber(x - y),
+                                ),
+                            );
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful total order", () => {
+                expectLawfulOrd(fc.float32Array());
+            });
         });
 
-        specify("#[Semigroup.cmb]", () => {
-            fc.assert(
-                fc.property(fc.float32Array(), fc.float32Array(), (xs, ys) => {
-                    const result = cmb(xs, ys);
+        describe("#[Semigroup.cmb]", () => {
+            it("combines the arrays using concatenation", () => {
+                fc.assert(
+                    fc.property(
+                        fc.float32Array(),
+                        fc.float32Array(),
+                        (xs, ys) => {
+                            const result = cmb(xs, ys);
 
-                    const exp = new Float32Array(xs.length + ys.length);
-                    exp.set(xs);
-                    exp.set(ys, xs.length);
+                            const exp = new Float32Array(xs.length + ys.length);
+                            exp.set(xs);
+                            exp.set(ys, xs.length);
 
-                    expect(result).to.deep.equal(exp);
-                    expect(exp.length).to.equal(xs.length + ys.length);
-                }),
-            );
+                            expect(result).to.deep.equal(exp);
+                            expect(exp.length).to.equal(xs.length + ys.length);
+                        },
+                    ),
+                );
+            });
+
+            it("implements a lawful semigroup", () => {
+                expectLawfulSemigroup(fc.float32Array());
+            });
         });
     });
 });
